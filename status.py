732838dd5cee76aca33be8c62e833f90cacea838
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-10-14 21:12:28 -0400

    pythonified

---
+++ status.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import httpx
+
+app = Flask(__name__)
+
+services = [
+    {
+        'name': 'Library website',
+        'url': 'https://library.kbcc.cuny.edu/homepage',
+        'status': None,
+    },
+    {
+        'name': 'ILLiad',
+        'url': 'https://kbcc.illiad.oclc.org/illiad/logon.html',
+        'status': None,
+    },
+    {
+        'name': 'dummy',
+        'url': 'https://library.kbcc.cuny.edu/dummy',
+        'status': None,
+    }
+]
+
+@app.route('/')
+def index():
+    for service in services:
+        try:
+            response = httpx.get(service['url'])
+            service['status'] = response.status_code
+        except:
+            service['status'] = 500
+
+    print(services)
+    return render_template('index.html', services=services)
+
+if __name__ == "__main__":
+    app.run(port=8000, host="127.0.0.1")
