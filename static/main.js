29ab98984ef6c61b2113ce33473046c304aa3b19
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-10-14 20:16:00 -0400

    further on js approach

--- static/main.js
+++ static/main.js
@@ -2,8 +2,8 @@ const statusComponent = {
         template: '<a v-for="service in services" :key="service.id">blah</a>',
         methods: {
                 checkStatus(site) {
-                        console.log("function!");
-                        fetch(site).then(function (response) {
+                        axios.get(site).then(function (response) {
+                                console.log(response.json);
                                 if (response.status == 200) {
                                         console.log("UP");
                                         return;
@@ -11,10 +11,11 @@ const statusComponent = {
                                         console.log("DOWN");
                                         return;
                                 }
+                        }).catch(function (error) {
+                                console.log(error.toJSON());
                         });
                 },
         },
-        props: ["services"],
         data() {
                 return {
                         services: [
@@ -29,6 +30,12 @@ const statusComponent = {
                                         site: "https://kbcc-cuny.illiad.oclc.org/illiad/logon.html",
                                         siteStatus: this.checkStatus("https://kbcc-cuny.illiad.oclc.org/illiad/logon.html"),
                                         id: 2,
+                                },
+                                {
+                                        name: "dummy",
+                                        site: "https://library.kbcc.cuny.edu/fajlfk",
+                                        siteStatus: this.checkStatus("https://library.kbcc.cuny.edu/fajlfk"),
+                                        id: 3,
                                 },
                         ],
                 }
